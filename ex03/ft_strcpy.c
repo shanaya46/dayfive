4f3b5ef6131211cbb72191a6a1d6eee3f8a703fb
Shanaya Jain <shjain@e1z2r7p26.42.us.org>
2018-07-17 11:11:37 -0700

    exercise 3

--- ex03/ft_strcpy.c
+++ ex03/ft_strcpy.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
-
+#include <string.h>
 
 char *ft_strcpy(char *dest, char *src) {
+int i;
+int len = strlen(src);
+for (i = 0; src[i] != '\0'; i++) {
+    dest[i] = src[i];
+}
 
+dest[i] = '\0';
+
+return dest;
 }
 
 int main () {
-    
+char src[10] = "Shanaya";
+char dest[10];
+
+printf("%s\n", ft_strcpy(dest, src));
+
 }
