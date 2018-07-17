8fca9b2aeb7664ee16a980680c7b4959af754412
Shanaya Jain <shjain@e1z2r7p26.42.us.org>
2018-07-17 13:15:52 -0700

    ex 10

---
+++ ex10/ft_strcapitalize.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+char *ft_strcapitalize(char *str) {
+
+}
+
+
+int main () {
+    int str[30] = "ABC DEF GHI";
+
+    printf("%s\n", ft_strcapitalize(str));
+}
