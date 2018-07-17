fd4c324c111015e2a4f0d3a30cb7308c96f3afc8
Shanaya Jain <shjain@e1z2r7p26.42.us.org>
2018-07-17 13:06:24 -0700

    lowercase

---
+++ ex09/ft_strlowcase.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char *ft_strlowcase(char *str) {
+
+for (int i = 0; str[i] != '\0'; i++) {
+    if(str[i] >= 'A' && str[i] <= 'Z') {
+        str[i] = str[i] + 32;
+    }
+}
+
+return str;
+
+}
+
+
+
+
+int main () {
+    char str[15] = "ABCDEFGHI";
+
+    printf("%s\n", ft_strlowcase(str));
+}
