bc8e1dac8273f7d9af00d242e63e115175052e11
Shanaya Jain <shjain@e1z2r7p26.42.us.org>
2018-07-18 09:55:11 -0700

    cap

--- ex10/ft_strcapitalize.c
+++ ex10/ft_strcapitalize.c
@@ -4,12 +4,36 @@
 
 
 char *ft_strcapitalize(char *str) {
+int i;
+int len = strlen(str);
+for (i = 1; i < len ; i++) {
+    if (i == 0){
+        if((str[i]>='a' && str[i]<='z')) {
+            str[i]=str[i]-32;
+        }
+    }
 
+    if(str[i]==' ') {
+        	i++;
+			if(str[i]>='a' && str[i]<='z')
+			{
+				str[i]=str[i]-32;
+			}
+    }
+    else {
+        if(str[i]>='A' && str[i]<='Z') {
+            str[i]=str[i] + 32;
+        }
+    }
+
+}
+
+return str;
 }
 
 
 int main () {
-    int str[30] = "ABC DEF GHI";
+    char str[30] = "ABC DEF GHI";
 
     printf("%s\n", ft_strcapitalize(str));
 }
