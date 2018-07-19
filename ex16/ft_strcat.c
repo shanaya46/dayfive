881192f68516d1455d0749f2ddafcf1bd73f329a
Shanaya Jain <shjain@e1z2r7p26.42.us.org>
2018-07-19 11:01:43 -0700

    strcat

--- ex16/ft_strcat.c
+++ ex16/ft_strcat.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-
+#include <string.h>
 
 char *ft_strcat(char *dest, char *src) {
+    int i, j;
+    j = strlen(dest);
 
+    for (i = 0; src[i] != '\0'; i++){
+        dest[j] = src[i];
+        j++;
+    }
+    return dest;
 }
 
 
 
 
 int main () {
-    char src[20] = "Shanaya";
-    char dest[20];
+    char src[20] = "source";
+    char dest[20] = "destination";
 
     printf("%s\n", ft_strcat(dest, src));
 }
