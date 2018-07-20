281dc77ca6d19ff0d223ca5bc34639307b447fc9
Shanaya Jain <shanayajain2001@gmail.com>
2018-07-19 19:42:07 -0700

    ex 19

--- ex18/ft_strlcat.c
+++ ex18/ft_strlcat.c
@@ -5,20 +5,18 @@ unsigned int ft_strlcat(char *dest, char *src, unsigned int size) {
     int i, j;
     j = strlen(dest);
 
-    for (i = 0; i < size; i++){
+    for (i = 0; i < size; i++, j++){
         dest[j] = src[i];
-        j++;
     }
     return strlen(dest);
 }
 
-
-
-
 int main () {
     char src[20] = "source";
     char dest[20] = "destination";
-    int size = 5;
+    int size = 10;
 
-    printf("Size of String: %d\n", ft_strlcat(dest, src, size));
+    printf("Source = '%s', Destination = '%s'\n", src, dest);
+    size = ft_strlcat(dest, src, size);
+    printf("Size of String '%s' is %d\n", dest, size);
 }
